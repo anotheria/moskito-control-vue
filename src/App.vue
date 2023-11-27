ba60ae2cff37d267efe79630dbc94acd648f8b5d
Grygorii Liashenko <g.lyashenko@gmail.com>
2023-11-28 01:20:20 +0200

    color schema update

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,16 @@
 <style lang="scss">
 :root {
   --mc-zinc-50: #fafafa;
-  --mc-zinc-100: #f3f4f6;
+  --mc-zinc-100: #f4f4f5;
+  --mc-zinc-200: #e4e4e7;
+  --mc-zinc-300: #d4d4d8;
+  --mc-zinc-400: #a1a1aa;
+  --mc-zinc-500: #71717a;
+  --mc-zinc-600: #52525b;
   --mc-zinc-700: #3f3f46;
-  --mc-zinc-900: #1c1917;
+  --mc-zinc-800: #27272a;
+  --mc-zinc-900: #18181b;
+  --mc-zinc-950: #09090b;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
